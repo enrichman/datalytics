979443c48f771e94997eb88a457ecd8d78ac0bbe
Federico Ginosa <f.ginosa@gmail.com>
2015-09-11 22:31:03 +0200

    fluxcomponent fix

--- client/components/Bacheca.jsx
+++ client/components/Bacheca.jsx
@@ -23,28 +23,16 @@ class Bacheca extends React.Component {
         <mui.List key={1} _grid={{x: 0, y: 0, w: 3, h: 4}}>
           <FluxComponent connectToStores={['datalytics']}>
             <RouteMenuItem icon="area-chart" route="my_analysis" text="Le mie analisi" />
-          </FluxComponent>
-          <FluxComponent connectToStores={['datalytics']}>
             <RouteMenuItem icon="bookmark" route="bookmark" text="Preferite" />
-          </FluxComponent>
-          <FluxComponent connectToStores={['datalytics']}>
             <RouteMenuItem icon="star" route="star" text="Popolari" />
-          </FluxComponent>
-          <FluxComponent connectToStores={['datalytics']}>
             <RouteMenuItem icon="question-circle" route="question" text="Assistenza" />
           </FluxComponent>
         </mui.List>
         <div className="RoutePage" key={2} _grid={{x: 3, y: 0, w: 5, h: 3}}>
           <FluxComponent connectToStores={['datalytics']}>
             <RoutePage name="my_analysis" page={<RouteMyAnalysis />} />
-          </FluxComponent>
-          <FluxComponent connectToStores={['datalytics']}>
             <RoutePage name="bookmark" page={<RoutePrefered />} />
-          </FluxComponent>
-          <FluxComponent connectToStores={['datalytics']}>
             <RoutePage name="star" page={<RoutePopular />} />
-          </FluxComponent>
-          <FluxComponent connectToStores={['datalytics']}>
             <RoutePage name="question" page={<RouteQuestion />}/>
           </FluxComponent>
         </div>
---
+++ client/components/RouteAnalysisDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RouteAnalysisDetails extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<h1>Analysis</h1>);
+  }
+
+}
+
+export default RouteAnalysisDetails;
